24e3de5fc23e5dac0599a12ebe4389da0a96b4e2
Samantha Miller <sm237@cs.washington.edu>
2022-03-25 14:50:33 -0700

    registered a scheduler

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -15,18 +15,18 @@ extern crate bento;
 pub mod enclave;
 pub mod ghost;
 
-use alloc::boxed::Box;
-use alloc::collections::vec_deque::VecDeque;
 use bento::println;
 
 use bento::bindings as c;
 use bento::kernel::ffi;
+use bento::kernel::raw;
 
 use enclave::LocalEnclave;
 use ghost::Ghost;
 
 struct BentoGhostModule {
     local_enclave: LocalEnclave,
+    agent: c::ghost_agent_type,
 }
 
 impl bento::KernelModule for BentoGhostModule {
@@ -40,9 +40,32 @@ impl bento::KernelModule for BentoGhostModule {
         };
         let mut map_size = 0;
         let local_enclave = LocalEnclave::new(ghost);
-        local_enclave.ghost.create_queue(1, 0, 0, &mut map_size);
-        Ok(BentoGhostModule{ local_enclave: local_enclave })
+        //let q_fd = local_enclave.ghost.create_queue(1, 0, 0, &mut map_size);
+        //let q_fd_struct = unsafe {
+        //    ffi::rs_fdget(q_fd as u32)
+        //};
+        //let ghost_q = unsafe {
+        //    ffi::rs_fd_to_queue(q_fd_struct)
+        //};
+        //unsafe {
+        //    (*ghost_q).process_message = Some(parse_message);
+        //}
+        unsafe {
+        //let agent_name = "bento\0";
+        let mut agent = c::ghost_agent_type {
+            policy: 10,
+            process_message: Some(parse_message),
+            owner: 0 as *mut c::module,
+            next: 0 as *mut c::ghost_agent_type
+        };
+        c::register_ghost_agent(&mut agent as *mut c::ghost_agent_type);
+        Ok(BentoGhostModule{ local_enclave: local_enclave, agent: agent })
+        }
     }
+}
+
+pub extern "C" fn parse_message(type_: i32, msglen: i32, barrier: u32, payload: *mut raw::c_void, payload_size: i32) {
+    println!("got message {}", type_);
 }
 
 impl Drop for BentoGhostModule {
